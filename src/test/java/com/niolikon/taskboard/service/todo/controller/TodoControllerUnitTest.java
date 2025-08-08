3e081b86dff03054cc445166d325e1e880f9f913
Simone Andrea Muscas <simoneandrea.muscas@gmail.com>
2025-08-08 20:58:00 +0200

    Improve readability and structure of tests
      TBS9

--- src/test/java/com/niolikon/taskboard/service/todo/controller/TodoControllerUnitTest.java
+++ src/test/java/com/niolikon/taskboard/service/todo/controller/TodoControllerUnitTest.java
@@ -27,125 +27,139 @@ import static org.springframework.http.HttpStatus.*;
 
 class TodoControllerUnitTest {
 
+    private Jwt stubJwt;
     private ITodoService todoService;
     private TodoController todoController;
 
     @BeforeEach
     void setUp() {
+        stubJwt = mock(Jwt.class);
+        when(stubJwt.getSubject()).thenReturn(JWT_SUBJECT_VALID_USER_ID);
+
         todoService = mock(ITodoService.class);
         todoController = new TodoController(todoService);
     }
 
     @Test
     void givenValidInput_whenCreateTodo_thenCreatedIsReturned() {
-        Jwt stubJwt = mock(Jwt.class);
-        when(stubJwt.getSubject()).thenReturn(JWT_SUBJECT_VALID_USER_ID);
+        // Arrange
         TodoRequest request = todoRequest_valid_fromClient;
         TodoView createdTodo = todoView_expected_fromTodoRequest;
-        when(todoService.create(anyString(), any(TodoRequest.class))).thenReturn(createdTodo);
+        when(todoService.create(JWT_SUBJECT_VALID_USER_ID, request)).thenReturn(createdTodo);
+
         ServletUriComponentsBuilder uriComponentsBuilder = mock(ServletUriComponentsBuilder.class);
         when(uriComponentsBuilder.path(TodoApiPaths.MAPPING_PATH_TODO_BY_ID)).thenReturn(uriComponentsBuilder);
         UriComponents uriComponents = mock(UriComponents.class);
         when(uriComponentsBuilder.buildAndExpand(createdTodo.getId())).thenReturn(uriComponents);
-        URI mockUri = new UriTemplate(TodoApiPaths.API_PATH_TODO_BY_ID).expand(createdTodo.getId());
-        when(uriComponents.toUri()).thenReturn(mockUri);
+        URI expectedLocation = new UriTemplate(TodoApiPaths.API_PATH_TODO_BY_ID).expand(createdTodo.getId());
+        when(uriComponents.toUri()).thenReturn(expectedLocation);
 
+        // Act
         ResponseEntity<TodoView> response = todoController.create(stubJwt, request, uriComponentsBuilder);
 
+        // Assert
         assertThat(response.getStatusCode()).isEqualTo(CREATED);
         assertThat(response.getBody()).isEqualTo(createdTodo);
-        verify(todoService, times(1)).create(JWT_SUBJECT_VALID_USER_ID, request);
+        assertThat(response.getHeaders().getLocation()).isEqualTo(expectedLocation);
+        verify(todoService).create(JWT_SUBJECT_VALID_USER_ID, request);
     }
 
     @Test
     void givenValidInput_whenReadAllTodos_thenOkIsReturned() {
-        Jwt stubJwt = mock(Jwt.class);
-        when(stubJwt.getSubject()).thenReturn(JWT_SUBJECT_VALID_USER_ID);
+        // Arrange
         List<TodoView> todos = List.of(todoView_instance1_fromRepository, todoView_instance2_fromRepository);
         Page<TodoView> todosPage = new PageImpl<>(todos, pageable_firstPageSize10_fromClient, todos.size());
         PageResponse<TodoView> todosPageResponse = new PageResponse<>(todosPage);
         when(todoService.readAll(anyString(), eq(pageable_firstPageSize10_fromClient))).thenReturn(todosPageResponse);
 
+        // Act
         ResponseEntity<PageResponse<TodoView>> response = todoController.readAll(stubJwt, pageable_firstPageSize10_fromClient);
 
+        // Assert
         assertThat(response.getStatusCode()).isEqualTo(OK);
         assertThat(response.getBody()).isEqualTo(todosPageResponse);
-        verify(todoService, times(1)).readAll(JWT_SUBJECT_VALID_USER_ID, pageable_firstPageSize10_fromClient);
+        verify(todoService).readAll(JWT_SUBJECT_VALID_USER_ID, pageable_firstPageSize10_fromClient);
     }
 
     @Test
     void givenValidInput_whenReadTodo_thenOkIsReturned() {
-        Jwt stubJwt = mock(Jwt.class);
-        when(stubJwt.getSubject()).thenReturn(JWT_SUBJECT_VALID_USER_ID);
+        // Arrange
         TodoView todo = todoView_instance1_fromRepository;
         Long todoId = todoView_instance1_fromRepository.getId();
         when(todoService.read(anyString(), eq(todoId))).thenReturn(todo);
 
+        // Act
         ResponseEntity<TodoView> response = todoController.read(stubJwt, todoId);
 
+        // Assert
         assertThat(response.getStatusCode()).isEqualTo(OK);
         assertThat(response.getBody()).isEqualTo(todo);
-        verify(todoService, times(1)).read(JWT_SUBJECT_VALID_USER_ID, todoId);
+        verify(todoService).read(JWT_SUBJECT_VALID_USER_ID, todoId);
     }
 
     @Test
     void givenValidInput_whenUpdateTodo_thenOkIsReturned() {
-        Jwt stubJwt = mock(Jwt.class);
-        when(stubJwt.getSubject()).thenReturn(JWT_SUBJECT_VALID_USER_ID);
+        // Arrange
         Long todoId = 1L;
         TodoRequest updateRequest = todoRequest_valid_fromClient;
         TodoView updatedTodo = todoView_expected_fromTodoRequest;
         when(todoService.update(anyString(), eq(todoId), any(TodoRequest.class))).thenReturn(updatedTodo);
 
+        // Act
         ResponseEntity<TodoView> response = todoController.update(stubJwt, todoId, updateRequest);
 
+        // Assert
         assertThat(response.getStatusCode()).isEqualTo(OK);
         assertThat(response.getBody()).isEqualTo(updatedTodo);
-        verify(todoService, times(1)).update(JWT_SUBJECT_VALID_USER_ID, todoId, updateRequest);
+        verify(todoService).update(JWT_SUBJECT_VALID_USER_ID, todoId, updateRequest);
     }
 
     @Test
     @Tag("Story=TBS1")
     void givenValidInput_whenPatchTodo_thenOkIsReturned() {
-        Jwt stubJwt = mock(Jwt.class);
-        when(stubJwt.getSubject()).thenReturn(JWT_SUBJECT_VALID_USER_ID);
+        // Arrange
         Long todoId = 1L;
         TodoPatch patchRequest = todoPatch_done_fromClient;
         TodoView updatedTodo = todoView_expected_fromTodoPatchDone;
         when(todoService.patch(anyString(), eq(todoId), any(TodoPatch.class))).thenReturn(updatedTodo);
 
+        // Act
         ResponseEntity<TodoView> response = todoController.patch(stubJwt, todoId, patchRequest);
 
+        // Assert
         assertThat(response.getStatusCode()).isEqualTo(OK);
         assertThat(response.getBody()).isEqualTo(updatedTodo);
-        verify(todoService, times(1)).patch(JWT_SUBJECT_VALID_USER_ID, todoId, patchRequest);
+        verify(todoService).patch(JWT_SUBJECT_VALID_USER_ID, todoId, patchRequest);
     }
 
     @Test
     @Tag("Story=TBS1")
     void givenValidInput_whenReadAllPendingTodos_thenOkIsReturned() {
-        Jwt stubJwt = mock(Jwt.class);
-        when(stubJwt.getSubject()).thenReturn(JWT_SUBJECT_VALID_USER_ID);
+        // Arrange
         List<TodoView> pendingTodos = List.of(todoView_pending1_fromRepository, todoView_pending2_fromRepository);
         Page<TodoView> pendingTodosPage = new PageImpl<>(pendingTodos, pageable_firstPageSize10_fromClient, pendingTodos.size());
         PageResponse<TodoView> pendingTodosPageResponse = new PageResponse<>(pendingTodosPage);
         when(todoService.readAllPending(anyString(), eq(pageable_firstPageSize10_fromClient))).thenReturn(pendingTodosPageResponse);
 
+        // Act
         ResponseEntity<PageResponse<TodoView>> response = todoController.readAllPending(stubJwt, pageable_firstPageSize10_fromClient);
 
+        // Assert
         assertThat(response.getStatusCode()).isEqualTo(OK);
         assertThat(response.getBody()).isEqualTo(pendingTodosPageResponse);
-        verify(todoService, times(1)).readAllPending(JWT_SUBJECT_VALID_USER_ID, pageable_firstPageSize10_fromClient);
+        verify(todoService).readAllPending(JWT_SUBJECT_VALID_USER_ID, pageable_firstPageSize10_fromClient);
     }
 
     @Test
     void givenValidInput_whenDeleteTodo_thenOkIsReturned() {
-        Jwt stubJwt = mock(Jwt.class);
-        when(stubJwt.getSubject()).thenReturn(JWT_SUBJECT_VALID_USER_ID);
+        // Arrange
         Long todoId = 1L;
+
+        // Act
         ResponseEntity<Void> response = todoController.delete(stubJwt, todoId);
 
+        // Assert
         assertThat(response.getStatusCode()).isEqualTo(NO_CONTENT);
-        verify(todoService, times(1)).delete(JWT_SUBJECT_VALID_USER_ID, todoId);
+        verify(todoService).delete(JWT_SUBJECT_VALID_USER_ID, todoId);
     }
 }
